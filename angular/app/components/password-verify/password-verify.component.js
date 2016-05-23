ca5220db8eb4b89e92625c596ba088ab79f4e1d1
alex quiambao <alexquiambao@yahoo.com>
2016-05-23 16:01:04 +0800

    Added forgot password / reset password module

--- angular/app/components/password-verify/password-verify.component.js
+++ angular/app/components/password-verify/password-verify.component.js
@@ -1,33 +1,33 @@
 function passwordVerifyClass () {
   return {
-    require: "ngModel",
+    require: 'ngModel',
     scope: {
       passwordVerify: '='
     },
-    link: function(scope, element, attrs, ctrl) {
-      scope.$watch(function() {
-        var combined;
+    link: function (scope, element, attrs, ctrl) {
+      scope.$watch(function () {
+        var combined
 
         if (scope.passwordVerify || ctrl.$viewValue) {
-          combined = scope.passwordVerify + '_' + ctrl.$viewValue;
+          combined = scope.passwordVerify + '_' + ctrl.$viewValue
         }
 
-        return combined;
-      }, function(value) {
+        return combined
+      }, function (value) {
         if (value) {
-          ctrl.$parsers.unshift(function(viewValue) {
-            var origin = scope.passwordVerify;
+          ctrl.$parsers.unshift(function (viewValue) {
+            var origin = scope.passwordVerify
 
             if (origin !== viewValue) {
-              ctrl.$setValidity("passwordVerify", false);
-              return undefined;
+              ctrl.$setValidity('passwordVerify', false)
+              return undefined
             } else {
-              ctrl.$setValidity("passwordVerify", true);
-              return viewValue;
+              ctrl.$setValidity('passwordVerify', true)
+              return viewValue
             }
-          });
+          })
         }
-      });
+      })
     }
   }
 }
