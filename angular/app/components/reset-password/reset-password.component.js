d8d2e02cbbfdac448bd0494a4b626a31dc11eef7
Ionut Tanasa <ionutz2k@gmail.com>
2016-06-07 06:01:20 +0200

    Reset password form improvements (#35)
    * reset-password improvements

--- angular/app/components/reset-password/reset-password.component.js
+++ angular/app/components/reset-password/reset-password.component.js
@@ -11,6 +11,7 @@ class ResetPasswordController {
     this.password = ''
     this.password_confirmation = ''
     this.isValidToken = false
+    this.formSubmitted = false
 
     this.verifyToken()
   }
@@ -27,26 +28,30 @@ class ResetPasswordController {
     })
   }
 
-  submit () {
-    this.alerts = []
-    let data = {
-      email: this.$state.params.email,
-      token: this.$state.params.token,
-      password: this.password,
-      password_confirmation: this.password_confirmation
+  submit (isValid) {
+    if(isValid) {
+      this.alerts = []
+      let data = {
+        email: this.$state.params.email,
+        token: this.$state.params.token,
+        password: this.password,
+        password_confirmation: this.password_confirmation
+      }
+
+      this.API.all('auth/password/reset').post(data).then(() => {
+        this.$state.go('login', {successMsg: `Your password has been changed, You may now login.`})
+      }, (res) => {
+        let alrtArr = []
+
+        angular.forEach(res.data.errors, function (value) {
+            alrtArr = {type: 'error', 'title': 'Error!', msg: value[0]}
+        })
+
+        this.alerts.push(alrtArr)
+        })
+    } else {
+        this.formSubmitted = true
     }
-
-    this.API.all('auth/password/reset').post(data).then(() => {
-      this.$state.go('login', { successMsg: `Your password has been changed, You may now login.` })
-    }, (res) => {
-      let alrtArr = []
-
-      angular.forEach(res.data.errors, function (value) {
-        alrtArr = { type: 'error', 'title': 'Error!', msg: value[0]}
-      })
-
-      this.alerts.push(alrtArr)
-    })
   }
 }
 
