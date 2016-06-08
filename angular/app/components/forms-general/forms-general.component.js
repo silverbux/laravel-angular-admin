4a124ae1d19f6fb16691c9d42578cfb44187dc36
Alex Quiambao <silverbux@users.noreply.github.com>
2016-06-08 13:04:41 +0800

    Added general form component and fixed html files indentionn to 2 (#36)

---
+++ angular/app/components/forms-general/forms-general.component.js
@@ -0,0 +1,19 @@
+class FormsGeneralController{
+    constructor(){
+        'ngInject';
+
+        //
+    }
+
+    $onInit(){
+    }
+}
+
+export const FormsGeneralComponent = {
+    templateUrl: './views/app/components/forms-general/forms-general.component.html',
+    controller: FormsGeneralController,
+    controllerAs: 'vm',
+    bindings: {}
+}
+
+
---
+++ tests/angular/app/components/forms-general.component.spec.js
@@ -0,0 +1,11 @@
+ngDescribe({
+    name: 'Test forms-general component',
+    modules: 'app',
+    element: '<forms-general></forms-general>',
+    tests: function (deps) {
+
+        it('basic test', () => {
+            //
+        });
+    }
+});
