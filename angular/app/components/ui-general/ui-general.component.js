fca382deeff8f5545f2a4a03302ca7f815047b83
alex quiambao <alexquiambao@yahoo.com>
2016-06-08 13:34:17 +0800

    Added general ui component

---
+++ angular/app/components/ui-general/ui-general.component.js
@@ -0,0 +1,16 @@
+class UiGeneralController {
+  constructor () {
+    'ngInject'
+
+  //
+  }
+
+  $onInit () {}
+}
+
+export const UiGeneralComponent = {
+  templateUrl: './views/app/components/ui-general/ui-general.component.html',
+  controller: UiGeneralController,
+  controllerAs: 'vm',
+  bindings: {}
+}
