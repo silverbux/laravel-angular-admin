8f7f17c75c0187f860e5b098c3ef5829639ea4c5
Alex Quiambao <silverbux@users.noreply.github.com>
2016-06-08 15:41:43 +0800

    Added ui buttons component (#39)

---
+++ tests/angular/app/components/ui-buttons.component.spec.js
@@ -0,0 +1,10 @@
+ngDescribe({
+  name: 'Test ui-buttons component',
+  modules: 'app',
+  element: '<ui-buttons></ui-buttons>',
+  tests: function (deps) {
+    it('basic test', () => {
+      //
+    })
+  }
+})
