edf8c77220befe83cbd6b10c1fa463b8d8c76738
Zach Mowrey <zbmowrey@gmail.com>
2016-10-02 00:44:24 -0400

    Fix oauth_provider and oauth_provider_id being set to null (#77)
    * Fix oauth_provider and oauth_provider_id being set to null
    
    Current User model sets oauth_provider and provider_id to null, rather than expected values; this situation prevents repeat log-in using any oauth method (creates a user with no password and no oauth_provider). This patch resolves.
    
    * Update for CI check
    
    Updating initial pull request

--- app/User.php
+++ app/User.php
@@ -22,7 +22,7 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'avatar',
+        'name', 'email', 'password', 'avatar', 'oauth_provider', 'oauth_provider_id',
     ];
 
     /**
